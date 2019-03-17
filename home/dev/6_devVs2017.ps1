5eaaa707ebaff52f2403e6edb6b9f8aba0ad90d3
gennesseaux <jocelyn@gennesseaux.fr>
2019-03-17 14:23:00 +0100

    VS2017 install

---
+++ home/dev/6_devVs2017.ps1
@@ -0,0 +1,75 @@
+#Requires -version 3
+#Requires -RunAsAdministrator
+
+# Dot source all functions
+. "$PSScriptRoot\..\..\.functions\.pack.ps1"
+
+# Chocolatey must be installed
+Install-Chocolatey
+
+# Profiles to install
+$profiles = @(
+    'DevCore'
+)
+
+# Define options
+$options = @(
+    'Boxstarter::Begin=false'
+    'Boxstarter::End=false'
+
+    'Boxstarter::DevCore::git=true'
+    'Boxstarter::DevCore::git-fork=true'
+    'Boxstarter::DevCore::poshgit=true'
+    'Boxstarter::DevCore::winmerge=true'
+    'Boxstarter::DevCore::cmder=true'
+    'Boxstarter::DevCore::wget=true'
+    'Boxstarter::DevCore::curl=true'
+    'Boxstarter::DevCore::cmake=true'
+    'Boxstarter::DevCore::VisualStudioCode=true'
+    'Boxstarter::DevCore::VisualStudioCodeExtensions=true'
+    'Boxstarter::DevCore::fonts=true'
+    
+
+    'Boxstarter::DevVs2017::Params=--locale fr-FR'
+
+    'Boxstarter::DevVs2017::Community=false'
+    'Boxstarter::DevVs2017::Professional=false'
+    'Boxstarter::DevVs2017::Entreprise=true'
+
+    'Boxstarter::DevVs2017::vswhere=true'
+
+    'Boxstarter::DevVs2017::azure=false'
+    'Boxstarter::DevVs2017::data=false'
+    'Boxstarter::DevVs2017::datascience=false'
+    'Boxstarter::DevVs2017::manageddesktop=false'
+    'Boxstarter::DevVs2017::managedgame=false'
+    'Boxstarter::DevVs2017::nativecrossplat=false'
+    'Boxstarter::DevVs2017::nativedesktop=true'
+    'Boxstarter::DevVs2017::nativegame=false'
+    'Boxstarter::DevVs2017::nativemobile=false'
+    'Boxstarter::DevVs2017::netcoretools=false'
+    'Boxstarter::DevVs2017::netcrossplat=false'
+    'Boxstarter::DevVs2017::netweb=false'
+    'Boxstarter::DevVs2017::node=false'
+    'Boxstarter::DevVs2017::office=false'
+    'Boxstarter::DevVs2017::python=false'
+    'Boxstarter::DevVs2017::universal=false'
+    'Boxstarter::DevVs2017::visualstudioextension=false'
+    'Boxstarter::DevVs2017::webcrossplat=false'
+)
+
+# Scripts
+$scripts = @()
+
+# Packages
+$packages = @()
+
+# Download my boxstarter bootstrap
+$webClient = New-Object System.Net.WebClient
+$webClient.DownloadFile('https://raw.githubusercontent.com/AbsCoDes/boxstarter/master/boxstarter.ps1', "$($env:temp)\boxstarter.ps1")
+
+# Start the setup process
+&Invoke-Command -ScriptBlock {
+    &"$($env:temp)\boxstarter.ps1" -profiles $profiles -options $options -scripts $scripts -packages $packages -webClient $webClient
+}
+
